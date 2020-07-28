935202a616245e8d5c9c07e98b027d447eeebea2
pecholleyc <cholley@chalmers.se>
2020-07-28 11:19:02 +0200

    feat: add mouse gem in csv MA-840
    remove database_name in csv, change its value using short_name. Ideally the database_name field should be completely removed from the model objects

--- frontend/src/api/models.js
+++ frontend/src/api/models.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const fetchModels = async () => {
   const { data } = await axios.get('repository/integrated_models/');
-  return data;
+  return data.map(m => Object({ ...m, database_name: `${m.short_name.toLowerCase()}` }));
 };
 
 export default { fetchModels };
