a3a6e9b5f34cc2074b8c1c661dd05003dd2e21a9
Mihail Anton <mihail.anton@chalmers.se>
2020-08-27 17:26:47 +0300

    feat: finish migration of ftp server MA-650

--- proj.sh
+++ proj.sh
@@ -3,8 +3,9 @@ export PATH=$PATH:/usr/local/bin
 
 function generate-data {
   echo 'Data generation started.'
-  source frontend/.env && yarn --cwd $DATA_GENERATOR_PATH start $DATA_FILES_PATH "$@" && cp -r $DATA_GENERATOR_PATH/data/ ./neo4j/import
-  cp -r $DATA_FILES_PATH/ftp-models ftp/ftp-models
+  source .env && yarn --cwd $DATA_GENERATOR_PATH start $DATA_FILES_PATH "$@" && cp -r $DATA_GENERATOR_PATH/data/ ./neo4j/import
+  cp -r $DATA_FILES_PATH/ftp-models ftp/
+  cp -r $DATA_FILES_PATH/svg api/
 }
 
 function build-stack {
