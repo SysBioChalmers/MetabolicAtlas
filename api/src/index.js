a2f6148d02338edb6a23a5737aaf54f67af949a5
Mihail Anton <mihail.anton@chalmers.se>
2020-10-26 23:50:30 +0200

    feat: api relies on nginx compression

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import router from 'endpoints/index';
-import compression from 'compression';
 
 const app = express();
 
-app.use(compression());
 app.use('/api/v2', router);
 app.use('/api/v2/public', express.static('public'));
 app.use('/api/v2/svg', express.static('svg'));
