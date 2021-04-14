1378d0ebc836441902ba24d9cdfa3e65e3c4f1eb
e0 <shan.huang@me.com>
2021-04-14 21:25:18 +0200

    fix: deploy cmd for prod env

--- proj.sh
+++ proj.sh
@@ -41,7 +41,7 @@ function logs {
 
 function deploy-stack {
   generate-data
-  docker-compose -f docker-compose.yml -f docker-compose-prod.yml --context $1 up -d --build
+  docker --context $1 compose -f docker-compose.yml -f docker-compose-local.yml logs -f nginx
 }
 
 function import-db {
