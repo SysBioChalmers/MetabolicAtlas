6d6080fd908d9636519f6670c5ade27d1f3ae156
Ingrid Hyltander <ingrid.hyltander@nbis.se>
2021-06-18 15:36:30 +0200

    refactor: adapt component subsystem to new maps available layout

--- api/src/neo4j/queries/subsystem.js
+++ api/src/neo4j/queries/subsystem.js
@@ -1,5 +1,5 @@
 import querySingleResult from 'neo4j/queryHandlers/single';
-import reformatExternalDbs from 'neo4j/shared/formatter';
+import { reformatExternalDbs } from 'neo4j/shared/formatter';
 import parseParams from 'neo4j/shared/helper';
 
 const getSubsystem = async ({ id, model, version }) => {
@@ -41,13 +41,20 @@ CALL apoc.cypher.run("
   UNION
   
   MATCH (:Subsystem${m} {id: '${id}'})-[${v}]-(ssvg:SvgMap)
-  RETURN { subsystemSVGs: COLLECT(DISTINCT(ssvg {.*})) } as data
+  WITH {subsystemId: '${id}', subsystemSVGs: COLLECT(DISTINCT(ssvg {.*}))} as subsystemSVG
+  RETURN { subsystemSVGs: COLLECT(subsystemSVG) } as data
 ", {}) yield value
 RETURN apoc.map.mergeList(COLLECT(value.data)) as subsystem
 `;
 
   const result = await querySingleResult(statement);
-  return { ...result, externalDbs: reformatExternalDbs(result.externalDbs) };
+  return { ...result, subsystemSVGs: [
+    {
+      id: result.id,
+      customName: result.name,
+      svgMaps: result.subsystemSVGs[0] ? result.subsystemSVGs[0].subsystemSVGs.sort((a, b) => a.id.localeCompare(b.id)) : [],
+    }
+  ], externalDbs: reformatExternalDbs(result.externalDbs) };
 };
 
 
