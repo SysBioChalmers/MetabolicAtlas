43f8cbb58b125ead2d4b71ac24a8138f9e619cab
e0 <shan.huang@me.com>
2021-07-13 18:31:25 +0200

    fix: clear selected element from map viewer if the new map does not contain it

--- frontend/src/components/explorer/mapViewer/Svgmap.vue
+++ frontend/src/components/explorer/mapViewer/Svgmap.vue
@@ -445,6 +445,7 @@ export default {
     },
     async selectElement(element, routeSelect = false) {
       if (!element) {
+        this.unSelectElement();
         return;
       }
       const [id, type] = this.getElementIdAndType(element);
